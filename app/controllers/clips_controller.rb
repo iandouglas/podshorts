2b3eeb6d0b960acc0e8052f1a615417af4e1d48e
ian douglas <ian.douglas@iandouglas.com>
2021-11-07 00:25:16 -0600

    can successfully get to edit page from index page

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -2,4 +2,18 @@ class ClipsController < ApplicationController
     def index
         @clips = Clip.all
     end
+
+    def edit
+        @clip = Clip.find(params[:id])
+
+        
+
+
+        # if @clip
+
+        # else 
+        #     # set error
+        #     rediect_to root_path
+        # end
+    end
 end
