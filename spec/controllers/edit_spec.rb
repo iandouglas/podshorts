852d5949a44d8c181ac290f1146a161b36e4de69
ian douglas <ian.douglas@iandouglas.com>
2021-11-07 01:57:02 -0600

    finished edit page verification of content loading properly

--- spec/controllers/edit_spec.rb
+++ spec/controllers/edit_spec.rb
@@ -8,9 +8,26 @@ RSpec.describe ClipsController, type: :request do
             visit edit_clip_path(clip.id)
 
             save_and_open_page
-            expect(page).to have_field('Video Filename', with: clip.video_filename)
             expect(page).to have_field('Processing finished?', checked: false)
+            expect(page).to have_field('Finished publishing?', checked: false)
+
+            expect(page).to have_field('Video Filename', with: clip.video_filename)
+            expect(page).to have_field('Clip Start', with: clip.clip_starttime)
+            expect(page).to have_field('Clip End', with: clip.clip_endtime)
+
             expect(page).to have_field('Do Podcast processing?', checked: true)
-        end
+            # expect(page).to have_field('Podcast Publication Date', with: clip.podcast_pub_datetime)
+            expect(page).to have_field('Podcast Title', with: clip.podcast_title)
+            expect(page).to have_field('Podcast Desc', with: clip.podcast_desc)
+            expect(page).to have_field('Podcast Teaser Filename', with: clip.podcast_teaser_filename)
+            expect(page).to have_field('Teaser Start', with: clip.podcast_teaser_starttime)
+            expect(page).to have_field('Teaser End', with: clip.podcast_teaser_endtime)
+
+            expect(page).to have_field('Do YouTube processing?', checked: true)
+            # expect(page).to have_field('YouTube Publication Date', with: clip.youtube_pub_datetime)
+            expect(page).to have_field('YouTube Title', with: clip.youtube_title)
+            expect(page).to have_field('YouTube Desc', with: clip.youtube_desc)
+            expect(page).to have_field('YouTube Thumbnail Text', with: clip.youtube_thumbnail_text)
+       end
     end
 end
